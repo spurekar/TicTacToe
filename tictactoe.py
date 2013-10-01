ad33a2a2d5bb4e3a45708a3821a0d40c1c152a3d
spurekarcareer <spurekarcareer@users.noreply.github.com>
2013-10-01 16:28:57 -0400

    Small commit to help with pairing

--- tictactoe.py
+++ tictactoe.py
@@ -4,12 +4,27 @@ import array
 
 #This function draws a board given the value of each cell
 def print_board(board):
-	print "Here is the current board:\n"
+	print "This is the current board:\n"
 	print ("     " + board[1] + ' | ' + board[2] + ' | ' + board[3])
 	print "     ---------"
 	print ("     " + board[4] + ' | ' + board[5] + ' | ' + board[6])
 	print "     ---------"
 	print ("     " + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+#This function handles the user's move
+def user_move():
+	while True:
+		#ask user for cell
+		cell = raw_input("Pick a cell to play (1-9): ")
+
+		#validate cell
+		try:
+			int(cell)
+			break
+		except:
+			print "Invalid input"
+	
+	#return cell number
 
 #This function checks each row, col, diagonal on the board
 def check_board(board):
@@ -22,6 +37,7 @@ def check_board(board):
 		return board[7]
 	else:
 		return ' '
+	
 	#check each col
 	if (board[1] == board[4] == board[7]):
 		return board[1]
@@ -41,10 +57,15 @@ def check_board(board):
 		return ' '
 	
 
-
 #Main
 
-print "\nWelcome to Tic Tac Toe\n"
+print "\nWelcome to Tic Tac Toe"
+print "Here is the number for each cell"
+print ("     " + '1' + ' | ' + '2' + ' | ' + '3')
+print "     ---------"
+print ("     " + '4' + ' | ' + '5' + ' | ' + '6')
+print "     ---------"
+print ("     " + '7' + ' | ' + '8' + ' | ' + '9')
 
 board = [' '] * 10
 
@@ -55,7 +76,11 @@ board[3] = 'x'
 #Create graphical board for user
 print_board(board)
 
-#TODO handle moves by user and computer
+#Handle user input
+user_move()
+
+
+#TODO handle computer plays
 
 #Check all combinations for a win
 done = check_board(board)
