8f95f500166718f7e172b5b3520f1b4828120603
spurekarcareer <spurekarcareer@users.noreply.github.com>
2013-10-02 14:06:49 -0400

    Working one-player version of tic tac toe.

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,6 @@
 #!/usr/local/bin/python2.7
 import array
 
-
 #This function draws a board given the value of each cell
 def print_board(board):
 	print "This is the current board:\n"
@@ -19,39 +18,38 @@ def user_move():
 
 		#validate cell
 		try:
-			int(cell)
-			break
-		except:
+			cell = int(cell)
+			if (cell >= 1 and cell <= 9):
+				#return cell number
+				return cell
+			else:
+				print "Invalid input"
+		except ValueError:
 			print "Invalid input"
 	
-	#return cell number
 
 #This function checks each row, col, diagonal on the board
-def check_board(board):
+def check_win(board):
 	#check each row
-	if (board[1] == board[2] == board[3]):
+	if (board[1] == board[2] == board[3] and board[1] != ' '):
 		return board[1]
-	elif (board[4] == board[5] == board[6]):
+	elif (board[4] == board[5] == board[6] and board[4] != ' '):
 		return board[4]
-	elif (board[7] == board[8] == board[9]):
+	elif (board[7] == board[8] == board[9] and board[7] != ' '):
 		return board[7]
-	else:
-		return ' '
 	
 	#check each col
-	if (board[1] == board[4] == board[7]):
+	elif (board[1] == board[4] == board[7] and board[1] != ' '):
 		return board[1]
-	elif (board[2] == board[5] == board[8]):
+	elif (board[2] == board[5] == board[8] and board[2] != ' '):
 		return board[2]
-	elif (board[3] == board[6] == board[9]):
+	elif (board[3] == board[6] == board[9] and board[3] != ' '):
 		return board[3]
-	else:
-		return ' '
 	
 	#check each diag
-	if (board[1] == board[5] == board[9]):
+	elif (board[1] == board[5] == board[9] and board[1] != ' '):
 		return board[1]
-	elif (board[7] == board[5] == board[3]):
+	elif (board[7] == board[5] == board[3] and board[7] != ' '):
 		return board[7]
 	else:
 		return ' '
@@ -68,26 +66,39 @@ print "     ---------"
 print ("     " + '7' + ' | ' + '8' + ' | ' + '9')
 
 board = [' '] * 10
-
-board[1] = 'x'
-board[2] = 'o'
-board[3] = 'x'
+movecount = 0
 
 #Create graphical board for user
 print_board(board)
 
-#Handle user input
-user_move()
+while True:
+
+	#Handle human move
+	cell = user_move()
+	movecount+= 1
+	board[cell] = 'X'
+
+	print_board(board)
+
+	#Handle computer plays
+	#cell = comp_move()
+
+	#Check all combinations for a win
+	done = check_win(board)
 
 
-#TODO handle computer plays
+	print board
 
-#Check all combinations for a win
-done = check_board(board)
+	#Exit if the game is over
+	if (done != ' ' or movecount == 9):
+		break
 
-#Handle if there is a winner
-if done == ' ':
-	print "There is no winner yet."
-else:
+
+if (done != ' '):
 	print "Yay! " + done + " has won the game."
+else:
+	print "There was a tie."
+
+
 print "\n"
+
